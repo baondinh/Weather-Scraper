9519749ef365ea88263d4a249ec5b33dd411c06a
Bao Dinh <49595165+baondinh@users.noreply.github.com>
2024-02-04 15:52:43 -0500

    Create weather_scraper.py to include the web-scraping method get_weather_forecast() from weather.py
    This file also contains test code

---
+++ weather_scraper.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import sys
+import forecast_parser as fp
+
+def get_weather_forecast(zip_code):
+    url = f"https://weather.com/weather/tenday/l/{zip_code}"
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for bad status codes
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    forecast_container = soup.find("div", class_=(re.compile("DailyForecast--DisclosureList")))
+
+    lst = []
+    for detail in forecast_container.find_all("details"):
+        lst.append(detail.find("div", class_=(re.compile("DetailsSummary"))).get_text(strip=True))
+
+    return lst
+
+if __name__ == "__main__":
+    zip_code = input("Enter 5-digit zip code: ")
+    try:
+        forecast = get_weather_forecast(zip_code)
+        parsed_data = fp.split_weather_line(forecast)
+        for line in parsed_data: 
+            print(line)
+
+        #Store data in a custom CSV file
+        file_name = f"weather_{zip_code}.csv"
+        fp.write_weather_csv(parsed_data, file_name)
+        print(f"Forecast data stored to {file_name}")
+    except:
+        print("Error")
+        sys.exit()
