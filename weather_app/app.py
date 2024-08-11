4a19f1a6f026c43654d5813597c7e6926396f76a
Bao Dinh <49595165+baondinh@users.noreply.github.com>
2024-08-11 12:03:36 -0400

    Remove need to access save files and temporarily pause graph generation

--- weather_app/app.py
+++ weather_app/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, send_file
-import os
+from flask import Flask, render_template, request
 from weather import get_weather_data
 
 app = Flask(__name__)
@@ -8,25 +7,18 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         zip_code = request.form['zip_code']
-        df, plot_filename, csv_filename = get_weather_data(zip_code)
-        
+        # df, plot_url = get_weather_data(zip_code)
+        df = get_weather_data(zip_code)
+
         if df is not None:
             return render_template('result.html', 
                                    data=df.to_html(classes='table table-striped', index=False),
-                                   plot=plot_filename,
-                                   csv=csv_filename,
+                                #    plot_url=plot_url,
                                    zip_code=zip_code)
         else:
             return render_template('index.html', error="An error occurred while fetching the weather data.")
     
     return render_template('index.html')
-
-@app.route('/download/<filename>')
-def download_file(filename):
-    if filename.endswith('.csv'):
-        return send_file(os.path.join('output', 'csv', filename), as_attachment=True)
-    elif filename.endswith('.png'):
-        return send_file(os.path.join('output', 'plots', filename), as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
